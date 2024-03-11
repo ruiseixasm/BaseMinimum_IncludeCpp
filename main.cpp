78ce1cb7df83b0036c6fe4a281d06ca95d2d57b7
Rui Monteiro <ruiseixas@yahoo.com>
2024-03-11 02:34:07 +0000

    Commands

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World!";
+    std::cout << "Hello World!" << std::endl;
     IncludeLib *lib_include = new IncludeLib();
     lib_include->PrintText();
     return 0;
